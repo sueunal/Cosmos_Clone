79913639d0cdf0246a075d8aea67f3b35e95fb60
sueunal <36671600+sueunal@users.noreply.github.com>
2023-05-18 01:01:47 +0900

    [➕] : ForEach와 Protocol로 리스트 기능 구현
    Protocol로 강의를 정의해 배열로 넣어 
    ForEach로 반복하여 기능 구현함

--- Cosmos_Clone/lectureView.swift
+++ Cosmos_Clone/lectureView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
-
+protocol Lecture{
+    var lecture_name : String { get }
+    var en_lecture_name : String { get }
+    var professor : String { get }
+}
 struct lectureView: View {
     @State private var lastLecture = Date()
-    struct lecture{
+    struct lecture : Lecture {
         let lecture_name : String
         let en_lecture_name : String
         let professor : String
@@ -22,37 +26,44 @@ struct lectureView: View {
     let std1 = Student(name: "김수은", std_id: "193806", department: "Information Security")
     let std1_lecture = lecture(lecture_name: "보안감사[00]", en_lecture_name: "Security Auditing", professor:"김현곤")
     
+    let my_lecture :[Lecture] = [lecture(lecture_name: "보안감사[00]", en_lecture_name: "Security Auditing", professor:"김현곤"),lecture(lecture_name: "침입탐지와차단[00]", en_lecture_name: "Intrusion Detection and Protection", professor:"김민수"),lecture(lecture_name: "캡스톤디자인II[03]", en_lecture_name: "Capstone Designl", professor:"서재현"),lecture(lecture_name: "인공지능과보안[00]", en_lecture_name: "Artificial Intelligence and Security", professor:"장봉석"),lecture(lecture_name: "취업과창업전략[03]", en_lecture_name: "Employment and Start-Up Strategy", professor:"강사"),]
+    private var i : Int? = nil
+    
+
+    
     var body: some View {
             NavigationStack {
-                ZStack(alignment: .leading){
-                    Color("DefaultColor")
-                        .edgesIgnoringSafeArea(.top)
-                        .frame(height: 120)
-                    HStack{
-                        Image("newyork")
-                            .resizable()
-                            .frame(width: 80,height: 80)
-                            .cornerRadius(40)
-                            .aspectRatio(contentMode: .fit)
-                            .padding()
-                        
-                        Text(std1.name)
-                            .padding(.bottom,30)
-                            .font(.title2)
-                    }
-                }
+//                ZStack(alignment: .leading){
+//                    Color("DefaultColor")
+//                        .edgesIgnoringSafeArea(.top)
+//                        .frame(height: 120)
+//                    HStack{
+//                        Image("newyork")
+//                            .resizable()
+//                            .frame(width: 80,height: 80)
+//                            .cornerRadius(40)
+//                            .aspectRatio(contentMode: .fit)
+//                            .padding()
+//
+//                        Text(std1.name)
+//                            .padding(.bottom,30)
+//                            .font(.title2)
+//                    }
+//                }
                 List{
                     Section {
+                        ForEach(my_lecture.indices,id:\.self){index in
                         NavigationLink{
                         }label: {
                             VStack(alignment: .leading){ // ForEach 처리 예정
-                                Text(std1_lecture.lecture_name)
-                                    .font(.title3)
-                                    .bold()
-                                Text(std1_lecture.en_lecture_name)
-                                    .foregroundColor(.gray)
-                                    .font(.caption2)
-                                    .bold()
+                                    Text(my_lecture[index].lecture_name)
+                                        .font(.title3)
+                                        .bold()
+                                    Text(my_lecture[index].en_lecture_name)
+                                        .foregroundColor(.gray)
+                                        .font(.caption2)
+                                        .bold()
+                                }
                             }
                         }
                     } header: {
@@ -65,6 +76,7 @@ struct lectureView: View {
                         DatePicker("과거 강좌를 조회합니다.", selection: $lastLecture,displayedComponents: .hourAndMinute)
                             .font(.title3)
                             .bold()
+                            .padding(1)
                     }
                 header: {
                     Text("과거 강좌")
